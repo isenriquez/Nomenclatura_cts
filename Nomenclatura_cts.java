d689c3c4112a644575619beee4a5c006184e6f06
isenriquez <65500311+isenriquez@users.noreply.github.com>
2020-06-12 22:10:15 -0500

    Nomenclatura_cts

---
+++ Nomenclatura_cts.java
@@ -0,0 +1,31 @@
+/*
+Implemente un algoritmo que nos permita ingresar la nomenclatura correcta de
+constantes, con el fin de estudiar la manera correcta de declarar cts, conforme
+a su paper.
+ */
+package nomenclatura_cts;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Israel Enriquez
+ */
+public class Nomenclatura_cts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner objeto=new Scanner(System.in);//new rerva espacio de memoria
+        System.out.println("UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+        System.out.println("NOMENCLATURA DE CONSTANTES");
+        System.out.println("GENERE VARIABLES USANDO LA NOMENCLATURA CORRECTA:");
+        String var1=objeto.nextLine();
+        String var2=objeto.nextLine();
+        String var3=objeto.nextLine();
+        String var4=objeto.nextLine();
+        String var5=objeto.nextLine();
+    }
+}
